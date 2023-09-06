f4308456b12fd19dd0816754ee323498d8eef98c
Jaffer Sheriff <jaffersheriff.u@zohocorp.com>
2023-09-06 16:40:37 +0530

    Initial Commit

---
+++ RSS Reader/RSS_ReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RSS_ReaderApp.swift
+//  RSS Reader
+//
+//  Created by Jaffer Sheriff U on 06/09/23.
+//
+
+import SwiftUI
+
+@main
+struct RSS_ReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
